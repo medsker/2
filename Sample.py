f2c594b5baee04b2281192e76c4c0ba4f7c3dec3
medsker <60238004+medsker@users.noreply.github.com>
2023-06-11 12:15:24 -0400

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -7,16 +7,17 @@ def load_json(file_path):
 
 def who_is_driver():
     '''Ask for the name of a driver.'''
-    driver_name = ''
-    while driver_name not in ['Jim', 'Joe']:
-        driver_name = input("What is the driver's name? ")
-    return driver_name
+    driver_name = input("What is the driver's name? ")
+    if driver_name in ['Jim', 'Joe']:
+        return driver_name
+    return None
 
 def where_is_driver(name, location_data):
     '''Check the location of a driver.'''
     if name in location_data:
         location = location_data[name]
         return name, location['lon'], location['lat']
+    return None
 
 def where_is_delivery(driver_name, driver_lon, driver_lat, delivery_data):
     '''Find deliveries near the driver and return a delivery manifest.'''
@@ -46,14 +47,18 @@ driver_location = {
 # Specify the name of the driver
 driver_name = who_is_driver()
 
-# Check the location of the driver
-driver_location_info = where_is_driver(driver_name, driver_location)
-if driver_location_info:
-    driver_name, driver_lon, driver_lat = driver_location_info
-    print(f"Driver {driver_name} is located at the following coordinates:\n{driver_lon}, {driver_lat}")
-    print(f"\nChecking to see if there is a delivery near {driver_name}...\n")
-    # Look for deliveries near the driver
-    driver_manifest = where_is_delivery(driver_name, driver_lon, driver_lat, data)
-    print(driver_manifest)
+# Check if a valid driver name was entered
+if driver_name is not None:
+    # Check the location of the driver
+    driver_location_info = where_is_driver(driver_name, driver_location)
+    if driver_location_info is not None:
+        driver_name, driver_lon, driver_lat = driver_location_info
+        print(f"Driver {driver_name} is located at the following coordinates:\n{driver_lon}, {driver_lat}")
+        print(f"\nChecking to see if there is a delivery near {driver_name}...\n")
+        # Look for deliveries near the driver
+        driver_manifest = where_is_delivery(driver_name, driver_lon, driver_lat, data)
+        #print(driver_manifest)
+    else:
+        print(f"Location not found for driver {driver_name}.")
 else:
-    print(f"Location not found for driver {driver_name}.")
+    print("Invalid driver name entered.")
